382fae5957ab5b318e172a0d2502ba800e0c3657
hdguff8 <3289131287@qq.com>
2019-05-06 22:18:41 +0800

    Update Cards.java

--- src/com/mckill/net/server/cardgroup/Cards.java
+++ src/com/mckill/net/server/cardgroup/Cards.java
@@ -4,17 +4,19 @@ import java.util.ArrayList;
 
 import com.mckill.card.AttC;
 import com.mckill.card.Card;
-import com.mckill.card.DefC;
 
 public class Cards {
 	public final static ArrayList<Card> Cards;
 	static {
+		// set card
 		Cards = new ArrayList<Card>();
-		Cards.add(new DefC(0, "def"));
-		Cards.add(new AttC(1, "att1", 1));
-		Cards.add(new AttC(2, "att10", 10));
-		Cards.add(new AttC(3, "att2", 2));
-		Cards.add(new AttC(4, "att3", 3));
+		// Cards.add(new DefC(0, "def"));
+		// Cards.add(new AttC(1, "att1", 1));
+		// Cards.add(new AttC(2, "att10", 10));
+		// Cards.add(new AttC(3, "att2", 2));
+		// Cards.add(new AttC(4, "att3", 3));
+
+		Cards.add(new AttC(1, "攻击", 1));
 	}
 
 	public static Card get(int Id) {
